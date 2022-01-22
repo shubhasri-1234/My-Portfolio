c3160f274124ba4e7b5ce5edfaff779ddc169e52
Shubhasri Tadepalli <83785024+shubhasri-1234@users.noreply.github.com>
2022-01-22 14:09:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+var typed = new Typed(".typing", {
+    strings: ["Front-End Developer","Designer","coder"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Machine Learning", "Full-Stack", "3D-Designing", "Graphic Designing"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+});
